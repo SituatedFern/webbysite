8e9d7f344778037ec7f89619b2a3a8c48e5b6630
SituatedFern <themareepevoloutions@gmail.com>
2022-01-25 11:20:49 -0500

    added wave on hover

--- components/Hero.js
+++ components/Hero.js
@@ -4,12 +4,19 @@ const Hero = () => {
   return (
     <div className="bg-black h-screen flex flex-col justify-center items-center font-bold font-poppins">
       {/* Add hover effect so that SVG hand waves when hovering over "Hey," */}
-      <h1 className="lg:text-9xl md:text-7xl sm:text-5xl text-3xl text-white hover:text-[#48C5F7] mb-14">
-        Hey,
-      </h1>
-      <h1 className="lg:text-9xl md:text-7xl sm:text-5xl text-3xl text-[#8FFFA8] mb-14 hover:text-[#FF4949] cursor-pointer">
+      <div className="lg:text-9xl md:text-7xl sm:text-5xl text-3xl mb-14">
+        <div className=" text-white hover:text-[#48C5F7] relative z-20 group">
+          <div className="h-20 lg:h-40 absolute left-0 right-0 flex justify-center items-center -z-10">
+            <p className="md:block hidden scale-0 text-white mr-72 lg:mr-[30rem] mb-3 lg:mb-10 group-hover:scale-100">
+              👋
+            </p>
+          </div>
+          <p className=" text-white hover:text-[#48C5F7]">Hey,</p>
+        </div>
+      </div>
+      <p className="lg:text-9xl md:text-7xl sm:text-5xl text-3xl text-[#8FFFA8] mb-14 hover:text-[#FF4949] cursor-pointer">
         I'm Ashwin!
-      </h1>
+      </p>
     </div>
   );
 };
