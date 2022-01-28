3de14fb28aa89002551384fed8fb3116ff55f8ee
SituatedFern <themareepevoloutions@gmail.com>
2022-01-27 23:14:24 -0500

    added wave animation and implemented it

--- components/Hero.js
+++ components/Hero.js
@@ -7,7 +7,8 @@ const Hero = () => {
       <div className="lg:text-9xl md:text-7xl sm:text-5xl text-3xl mb-14">
         <div className=" text-white hover:text-[#48C5F7] relative z-20 group">
           <div className="h-20 lg:h-40 absolute left-0 right-0 flex justify-center items-center -z-10">
-            <p className="md:block hidden scale-0 text-white mr-72 lg:mr-[30rem] mb-3 lg:mb-10 group-hover:scale-100 transition-all duration-150 origin-right">
+            {/* to remove wave get rid of "group-hover:animate-wave" */}
+            <p className="md:block hidden scale-0 text-white mr-72 lg:mr-[30rem] mb-3 lg:mb-10 group-hover:scale-100 group-hover:animate-wave transition-all duration-150 origin-right">
               👋
             </p>
           </div>
