f8f33b3ba39be7d76dc52f0674fd9419bae3d3a7
SituatedFern <themareepevoloutions@gmail.com>
2022-03-21 22:19:35 -0400

    made dropdown functional

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -6,21 +6,21 @@ const Dropdown = ({ isOpen, toggle }) => {
     <div
       className={
         isOpen
-          ? "grid grid-rows-4 text-center items-center bg-yellow-500"
+          ? "grid grid-rows-4 text-center items-center bg-gray-600"
           : "hidden"
       }
       onClick={toggle}
     >
-      <Link href="/">
+      <Link href="#About">
         <a className="p-4">About</a>
       </Link>
-      <Link href="/">
+      <Link href="#Portfolio">
         <a className="p-4">Portfolio</a>
       </Link>
-      <Link href="/">
+      <Link href="#Newsletter">
         <a className="p-4">Newsletter</a>
       </Link>
-      <Link href="/">
+      <Link href="#Footer">
         <a className="p-4">Contact</a>
       </Link>
     </div>
