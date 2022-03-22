823c64a1db5a845cf930f3f0eb626404919888ef
SituatedFern <themareepevoloutions@gmail.com>
2022-03-21 22:30:05 -0400

    styled dropdown to match theme

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -6,22 +6,22 @@ const Dropdown = ({ isOpen, toggle }) => {
     <div
       className={
         isOpen
-          ? "grid grid-rows-4 text-center items-center bg-gray-600"
+          ? "grid grid-rows-4 text-center items-center bg-[#0F0F0F]"
           : "hidden"
       }
       onClick={toggle}
     >
       <Link href="#About">
-        <a className="p-4">About</a>
+        <a className="p-4 font-jetbrains italic text-[#FF4949]">About</a>
       </Link>
       <Link href="#Portfolio">
-        <a className="p-4">Portfolio</a>
+        <a className="p-4 font-jetbrains italic text-[#FF4949]">Portfolio</a>
       </Link>
       <Link href="#Newsletter">
-        <a className="p-4">Newsletter</a>
+        <a className="p-4 font-jetbrains italic text-[#FF4949]">Newsletter</a>
       </Link>
       <Link href="#Footer">
-        <a className="p-4">Contact</a>
+        <a className="p-4 font-jetbrains italic text-[#FF4949]">Contact</a>
       </Link>
     </div>
   );
